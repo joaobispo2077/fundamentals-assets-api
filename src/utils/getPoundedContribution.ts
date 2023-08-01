7f7fb5bfdf96d28a8ad536a5419adcd35700740b
João Bispo <joaobispo2077@gmail.com>
2023-08-01 12:24:54 -0300

    feat: add get pounded contribution calculation

---
+++ src/utils/getPoundedContribution.ts
@@ -0,0 +1,8 @@
+export type Asset = {
+  ticker: string;
+  grade: number;
+};
+
+export const getPoundedContribution = (asset: Asset, totalContribution: number, sumAllStockGrades: number) => {
+  return (asset.grade * totalContribution) / sumAllStockGrades;
+}
