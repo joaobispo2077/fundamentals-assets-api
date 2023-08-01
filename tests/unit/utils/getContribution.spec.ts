855dad59f3bca3216b46c0498b8fe4728162bf1a
João Bispo <joaobispo2077@gmail.com>
2023-08-01 12:25:03 -0300

    test: add get pounded contribution calculation

---
+++ tests/unit/utils/getContribution.spec.ts
@@ -0,0 +1,27 @@
+import { Asset, getPoundedContribution } from "@src/utils/getPoundedContribution";
+
+describe("getContribution", () => {
+  it("should be able to calculate contribution per asset using 3 assets for SCHH", () => {
+    const assets: Asset[] = [
+      {
+        ticker: 'IJR',
+        grade: 1,
+      },
+      {
+        ticker: 'SCHH',
+        grade: 8,
+      },
+      {
+        ticker: 'VNQI',
+        grade: 1,
+      },
+    ];
+
+    const totalContribution = 100;
+    const sumAllStockGrades = assets.reduce((acc, asset) => acc + asset.grade, 0);
+
+    const contributionForSchh = getPoundedContribution(assets[1], totalContribution, sumAllStockGrades)
+
+    expect(contributionForSchh).toBe(80);
+  });
+});
